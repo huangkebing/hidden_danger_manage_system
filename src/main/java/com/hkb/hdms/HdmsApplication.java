c9d80a6528530a6435a9cf0254151a3b98b202fc
huangkeb <1216033824@qq.com>
2021-02-07 19:23:16 +0800

    init project

---
+++ src/main/java/com/hkb/hdms/HdmsApplication.java
@@ -0,0 +1,13 @@
+package com.hkb.hdms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HdmsApplication.class, args);
+    }
+
+}
