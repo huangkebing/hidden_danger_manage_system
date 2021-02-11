4cbff624b83df0171bd3dba8c961b96f03fe0d73
huangkeb <1216033824@qq.com>
2021-02-11 09:56:08 +0800

    初步集成activiti

---
+++ src/main/java/com/hkb/hdms/base/ActivitiDBCreate.java
@@ -0,0 +1,9 @@
+package com.hkb.hdms.base;
+
+import org.activiti.engine.impl.db.DbSchemaCreate;
+
+public class ActivitiDBCreate {
+    public static void main(String[] args) {
+        DbSchemaCreate.main(args);
+    }
+}
