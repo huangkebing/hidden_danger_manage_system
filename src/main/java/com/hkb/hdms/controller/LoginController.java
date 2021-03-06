f2620d9f5236581dcb165d92ac11581822b5770c
huangkeb <1216033824@qq.com>
2021-03-06 10:14:20 +0800

    3月5日进度，初步研究验证码登录

--- src/main/java/com/hkb/hdms/controller/LoginController.java
+++ src/main/java/com/hkb/hdms/controller/LoginController.java
@@ -1,13 +1,19 @@
 package com.hkb.hdms.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * 登录相关Controller
  * @author huangkebing
  * 2021/02/11
  */
-
-@RestController
+@Controller
 public class LoginController {
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        return "hello";
+    }
 }
