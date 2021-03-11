3be8005977dd3db96c503d475518bb1ee20a0bc2
huangkeb <1216033824@qq.com>
2021-03-11 18:56:26 +0800

    3月11日进度，spring security 授权

--- src/main/java/com/hkb/hdms/service/impl/UserServiceImpl.java
+++ src/main/java/com/hkb/hdms/service/impl/UserServiceImpl.java
@@ -4,13 +4,19 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hkb.hdms.mapper.UserMapper;
 import com.hkb.hdms.model.pojo.User;
+import com.hkb.hdms.model.pojo.UserRole;
+import com.hkb.hdms.service.RoleService;
 import com.hkb.hdms.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Collection;
 
 /**
  * @author huangkebing
@@ -19,12 +25,21 @@ import java.util.Collections;
 @Component
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
 
+    private final RoleService roleService;
+
+    @Autowired
+    public UserServiceImpl(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
     @Override
     public UserDetails loadUserByEmail(String email) {
         User queryUser = this.getOne(new QueryWrapper<User>().eq("email", email));
         UserDetails userDetails = null;
-        if(queryUser != null){
-            userDetails=new org.springframework.security.core.userdetails.User(email, new BCryptPasswordEncoder().encode("123456"), Collections.emptyList());
+        if (queryUser != null) {
+            Collection<GrantedAuthority> authorities = getAuthorities(queryUser);
+            userDetails = new org.springframework.security.core.userdetails.User(email,
+                    new BCryptPasswordEncoder().encode("123456"), authorities);
         }
         return userDetails;
     }
@@ -33,10 +48,21 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User queryUser = this.getOne(new QueryWrapper<User>().eq("email", username));
         UserDetails userDetails = null;
-        if(queryUser != null){
+        if (queryUser != null) {
             String password = queryUser.getPassword();
-            userDetails=new org.springframework.security.core.userdetails.User(username, new BCryptPasswordEncoder().encode(password), Collections.emptyList());
+            //获取权限
+            Collection<GrantedAuthority> authorities = getAuthorities(queryUser);
+            userDetails = new org.springframework.security.core.userdetails.User(username,
+                    new BCryptPasswordEncoder().encode(password), authorities);
         }
         return userDetails;
+    }
+
+    //获取用户角色
+    private Collection<GrantedAuthority> getAuthorities(User user) {
+        ArrayList<GrantedAuthority> authorities = new ArrayList<>();
+        UserRole role = roleService.getById(user.getRole());
+        authorities.add(new SimpleGrantedAuthority("ROLE_" + role.getName()));
+        return authorities;
     }
 }
