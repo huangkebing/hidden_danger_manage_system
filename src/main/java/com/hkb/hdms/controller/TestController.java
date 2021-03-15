eeeb9bd843ce5f3a466aa54d9217c0f879ac96e7
huangkeb <1216033824@qq.com>
2021-03-15 08:59:52 +0800

    3月14日进度，初步集成activiti7，使用简单流程先熟悉框架，初步集成

---
+++ src/main/java/com/hkb/hdms/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.hkb.hdms.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 初步集成activiti测试使用
+ *
+ * @author huangkebing
+ * 2021/03/15
+ */
+@Controller
+@RequestMapping("/test")
+public class TestController {
+}
--- src/test/java/com/hkb/hdms/HdmsApplicationTests.java
+++ src/test/java/com/hkb/hdms/HdmsApplicationTests.java
@@ -1,25 +1,13 @@
 package com.hkb.hdms;
 
-import com.hkb.hdms.mapper.RoleMapper;
-import com.hkb.hdms.model.pojo.UserRole;
-import com.hkb.hdms.service.RoleService;
-import com.hkb.hdms.utils.ValidateCodeMailSender;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HdmsApplicationTests {
 
-    @Autowired
-    private RoleMapper mapper;
-
     @Test
     void contextLoads() {
-        UserRole role = new UserRole();
-        role.setName("ADMIN");
-        role.setDescription("系统管理员");
-        mapper.insert(role);
     }
 
 }
