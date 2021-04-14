7251a3409a3a589697885c528a74c28c56232a27
huangkeb <1216033824@qq.com>
2021-04-14 16:01:11 +0800

    4月14日进度，完成了页面之间刷新，把弹窗尽量切换成子页面的形式具体等最后统一ui的时候去做。今天先把详情页做出来

--- src/main/resources/static/js/url.js
+++ src/main/resources/static/js/url.js
@@ -5,3 +5,16 @@ let localhostPath = curRequestPath.substring(0,ipAndPort);
 let projectName = pathName.substring(0,pathName.substr(1).indexOf('/')+1);
 
 let basePath = localhostPath + projectName;
+
+function generateUUID() {
+    var d = new Date().getTime();
+    if (window.performance && typeof window.performance.now === "function") {
+        d += performance.now();
+    }
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = (d + Math.random() * 16) % 16 | 0;
+        d = Math.floor(d / 16);
+        return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+    return uuid;
+}
